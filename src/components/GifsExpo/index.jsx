e422acb00398afb9b6340bbd2e7ab5b6e99b6dc5
hackerboy09 <isc1930319@gmail.com>
2023-05-25 09:48:32 -0600

    Mostrar imagenes de gifs y eliminar opciones de la list

--- src/components/GifsExpo/index.jsx
+++ src/components/GifsExpo/index.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const GifsExpo = ({ categories = [] }) => {
 
     const [urlList, setUrlList] = useState([])
 
     const getGifs = async (categories) => {
+    if (categories.length === 0) {
+        setUrlList([])
+        return
+
+    }    
+
 
         const responsesList = await Promise.all(categories.map(async (category) => {
             const response = await fetch(
@@ -25,7 +31,13 @@ const GifsExpo = ({ categories = [] }) => {
         setUrlList([...gifsList])
     }
 
-    getGifs(categories)
+  useEffect(() => {
+     getGifs(categories)
+  },
+  [categories]
+  )
+
+   
 
     return (
         <>
